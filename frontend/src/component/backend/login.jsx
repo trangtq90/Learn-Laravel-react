ce71f5877209db51d040f0ffd03e687873604477
trangtq90 <ate.trangtq1@gmail.com>
2025-08-29 19:21:41 +0700

    css form login

--- frontend/src/component/backend/login.jsx
+++ frontend/src/component/backend/login.jsx
@@ -4,46 +4,47 @@ import { Form, Button } from "react-bootstrap";
 
 const Login = () => {
   return (
-    <div className="d-flex flex-column min-vh-100">
-      {/* Header */}
-      <Header />
+    <>
+        <Header />
 
-      {/* Main content (canh giữa form login) */}
-      <main className="flex-grow-1 d-flex justify-content-center align-items-center">
-        <div className="card border-0 shadow" style={{ width: "350px" }}>
-          <div className="card-header text-center">
-            <h4>Login</h4>
-          </div>
-          <div className="card-body">
-            <Form>
-              <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-                <Form.Text className="text-muted">
-                  We'll never share your email with anyone else.
-                </Form.Text>
-              </Form.Group>
+        {/* Main */}
+        <main className="my-5">
+            <div className="container d-flex justify-content-center">
+                <div className="card border-0 shadow" style={{ width: "350px" }}>
+                <div className="card-header text-center">
+                    <h4>Login</h4>
+                </div>
+                <div className="card-body">
+                    <Form>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                        </Form.Text>
+                    </Form.Group>
 
-              <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-              </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
 
-              <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
-              </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                        <Form.Check type="checkbox" label="Check me out" />
+                    </Form.Group>
 
-              <Button variant="primary" type="submit" className="w-100">
-                Submit
-              </Button>
-            </Form>
-          </div>
-        </div>
-      </main>
+                    <Button variant="primary" type="submit" className="w-100">
+                        Submit
+                    </Button>
+                    </Form>
+                </div>
+                </div>
+            </div>
+        </main>
 
       {/* Footer */}
       <Footer />
-    </div>
+      </>
   );
 };
 
--- frontend/src/component/common/Header.jsx
+++ frontend/src/component/common/Header.jsx
@@ -6,24 +6,24 @@ import Logo from '../../assets/images/profile-img.jpg';
 const Header = () => {
   return (
     <header>
-        <Navbar expand="lg" className="fixed-top">
-          <div className='container py-3'>
-            <Navbar.Brand href="#home" className='logoText'>
-              <img src={Logo} alt="#" className='logoImage' />
-              <span className='px-2'>Lớp Toán Thầy Tráng </span>
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="ms-auto">
-                <Nav.Link href="#home" className='nav-link'>Home</Nav.Link>
-                <Nav.Link href="#link" className='nav-link'>Link</Nav.Link>
-                <Nav.Link href="/about" className='nav-link'>About</Nav.Link>
-              </Nav>
-            </Navbar.Collapse>
-          </div>
+		<Navbar>
+			<div className="container py-3">
+			<Navbar.Brand href="#home" className='logoText'>
+				<img src={Logo} alt="Logo" className='logoImage' />
+				<span className='px-2'>Lớp Toán Thầy Tráng</span>
+			</Navbar.Brand>
+			<Navbar.Toggle aria-controls="basic-navbar-nav" />
+			<Navbar.Collapse id="basic-navbar-nav">
+				<Nav className="ms-auto">
+				<Nav.Link href="#home" className='nav-link'>Home</Nav.Link>
+				<Nav.Link href="#link" className='nav-link'>Link</Nav.Link>
+				<Nav.Link href="/about" className='nav-link'>About</Nav.Link>
+				</Nav>
+			</Navbar.Collapse>
+			</div>
+		</Navbar>
+	</header>
 
-        </Navbar>
-      </header>
   )
 }
 
