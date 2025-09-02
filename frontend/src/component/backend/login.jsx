1c138d2271fb2d055cc11f5faaeb2dbfba1b0418
trangtq90 <ate.trangtq1@gmail.com>
2025-09-02 11:11:04 +0700

    validate form login admin

--- frontend/src/component/backend/login.jsx
+++ frontend/src/component/backend/login.jsx
@@ -1,8 +1,15 @@
 import Header from "../common/Header";
 import Footer from "../common/Footer";
-import { Form, Button } from "react-bootstrap";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
+    const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+    } = useForm()
+    const onSubmit = (data) => console.log(data);
   return (
     <>
         <Header />
@@ -10,34 +17,48 @@ const Login = () => {
         {/* Main */}
         <main className="my-5">
             <div className="container d-flex justify-content-center">
-                <div className="card border-0 shadow" style={{ width: "350px" }}>
-                <div className="card-header text-center">
-                    <h4>Login</h4>
-                </div>
-                <div className="card-body">
-                    <Form>
-                    <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Label>Email address</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
-                        <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                        </Form.Text>
-                    </Form.Group>
+                <div className="form-login ">
+                    <div className="card border-0 shadow">
+                        <div className="card-body p-4">
+                            <form onSubmit={handleSubmit(onSubmit)}>
+                                <h3 className="mb-4 text-center">Login</h3>
+                                <div className="mb-3">
+                                    <label htmlFor="" className="form-label">Email</label>
+                                    <input 
+                                        {...register("email", { 
+                                            required: 'Email không được để trống',
+                                            pattern: {
+                                                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                                message: "Email không đúng định dạng"
+                                            }
+                                        })}
+                                        type="text" 
+                                        placeholder="Enter email" 
+                                        className={`form-control ${errors.email && 'is-invalid'}`}
+                                    />
+                                    {errors.email && <span className="invalid-feedback">{errors.email.message}</span>}
+                                </div>
+                                <div className="mb-3">
+                                    <label htmlFor="" className="form-label">Password</label>
+                                    <input 
+                                        {...register("password", { 
+                                            required: 'Password không được để trống',
+                                            pattern: {
+                                            value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9]).{8,}$/,
+                                            message: "Mật khẩu phải ≥8 ký tự, có chữ hoa, chữ thường và ký tự đặc biệt",
+                                            },
+                                        })}
+                                        type="password" 
+                                        placeholder="Enter Password" 
+                                        className={`form-control ${errors.password && 'is-invalid'}`}
+                                    />
+                                    {errors.password && <span className="invalid-feedback">{errors.password.message}</span>}
+                                </div>
+                                <button type="submit" className="btn btn-primary">Login</button>
+                            </form>
 
-                    <Form.Group className="mb-3" controlId="formBasicPassword">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
-                    </Form.Group>
-
-                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                        <Form.Check type="checkbox" label="Check me out" />
-                    </Form.Group>
-
-                    <Button variant="primary" type="submit" className="w-100">
-                        Submit
-                    </Button>
-                    </Form>
-                </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </main>
